9c6bb3961ab812dd7921ef37b81daabbcf5bce4f
Caue Maldonado Lima <85191254+CaueMaldonadoLima@users.noreply.github.com>
2025-03-31 16:10:22 -0400

    Create LeetCode20.py

---
+++ LeetCode20.py
@@ -0,0 +1,20 @@
+#20. Valid Parentheses
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        close_map = { 
+            ")" : "(", 
+            "]" : "[", 
+            "}" : "{" 
+        }
+
+        for char in s:
+            if char in close_map:
+                if stack and stack[-1] == close_map[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        
+        return True if not stack else False
