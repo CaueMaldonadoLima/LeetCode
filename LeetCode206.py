99ba549d6059b1799a4558600d214f803d03b0ad
Caue Maldonado Lima <85191254+CaueMaldonadoLima@users.noreply.github.com>
2025-04-01 16:59:48 -0400

    Create LeetCode206.py

---
+++ LeetCode206.py
@@ -0,0 +1,18 @@
+#206. Reverse Linked List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+#Two Pointers Solution
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return  prev
